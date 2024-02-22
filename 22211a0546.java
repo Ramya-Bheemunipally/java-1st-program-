7009cc2a62aa16d8fb625cb4a240cc1090194021
Ramya-Bheemunipally <140195092+Ramya-Bheemunipally@users.noreply.github.com>
2024-02-22 11:49:20 +0530

    Add files via upload

---
+++ 22211a0546.java
@@ -0,0 +1,11 @@
+//this program is used to print hi 
+import java.io.*;
+class Demo1
+{
+public static void main(String args[ ])
+{
+System.out.println("hi");
+System.out.println("hi"+"\t"+"bye");
+System.out.println("hello");
+}//main
+}//Demo1
